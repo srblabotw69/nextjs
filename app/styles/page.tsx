80e8c7937620a790037e26f29ff51adbb980cb04
sspc <mus56789@gmail.com>
2024-07-29 13:40:36 -0400

    c

--- app/styles/page.tsx
+++ app/styles/page.tsx
@@ -1,129 +1,7 @@
-// import styled from "styled-components";
-// import { theme } from "../utils/theme";
-
-// export const OuterScreenBlockPadding = styled.div`
-//   padding: 1rem 2rem;
-//   box-sizing: border-box;
-//   height: 100%;
-
-//   @media only screen and (max-width: 800px) {
-//     padding: 0.5rem 0.5rem;
-//   }
-// `;
-
-// export const PageTitle = styled.div`
-//   font-family: "Montserrat", serif;
-//   font-style: normal;
-//   font-weight: 700;
-//   font-size: 20px;
-//   line-height: 24px;
-//   color: #000000;
-//   margin-top: 20px;
-//   margin-bottom: 20px;
-
-//   @media only screen and (max-width: 700px) {
-//     padding-left: 20px;
-//   }
-// `;
-
-// type ScreenOuterBlockProps = {
-//   width?: number;
-// };
-
-// export const ScreenOuterBlock = styled.div<ScreenOuterBlockProps>`
-//   margin: 0 auto;
-//   max-width: ${({ width }) => width ?? 1200}px;
-// `;
-
-// export const globalRadiusWhite = `
-//   border-radius: 8px;
-//   background: #ffffff;
-//   box-shadow: 0 4px 20px #e6e6e6;
-// `;
-
-// export const ScreenWhiteBlock = styled.div`
-//   padding: 2rem 3rem;
-//   box-sizing: border-box;
-//   ${globalRadiusWhite};
-
-//   @media only screen and (max-width: 750px) {
-//     padding: 0.5rem 0.5rem;
-//   }
-// `;
-
-// export const TableHolder = styled.div`
-//   margin-top: 12px;
-//   overflow-x: auto;
-// `;
-
-// export const PageHeader = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   margin-bottom: 8px;
-// `;
-
-// export const Alert = styled.div`
-//   background-color: ${theme.primary["orange-vivid-700"]};
-//   display: flex;
-//   padding: 10px 15px;
-//   justify-content: center;
-//   border-radius: 2px;
-//   color: #fff;
-//   align-items: center;
-//   margin-bottom: 1rem;
-//   box-sizing: border-box;
-// `;
-// export const ColumnContainer = styled.div`
-//   width: 700px;
-//   border-radius: 10px;
-
-//   @media (max-width: 700px) {
-//     width: 100%;
-//   }
-// `;
-// export const PostsContainer = styled.div`
-//   padding: 28px 46px;
-//   background-color: #fff;
-//   box-sizing: border-box;
-
-//   @media (max-width: 700px) {
-//     padding: 28px 0px 20px 20px;
-//   }
-// `;
-// export const PostBlockContainer = styled.div`
-//   box-shadow: 0 4px 33px rgba(168, 198, 207, 0.15);
-//   border-radius: 10px 10px 0 0;
-//   background-color: #fff;
-//   padding: 36px 36px 0 36px;
-
-//   @media (max-width: 700px) {
-//     padding: 36px 8px 0 8px;
-//     box-sizing: border-box;
-//   }
-// `;
-// export const BlackButton = styled.div`
-//   background: #000;
-//   border-radius: 15px;
-//   font-family: "Audiowide", serif;
-//   font-style: normal;
-//   font-weight: 400;
-//   font-size: 12px;
-//   line-height: 15px;
-//   color: #ffffff;
-//   padding: 0 10px;
-//   height: 36px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   cursor: pointer;
-//   text-transform: uppercase;
-// `;
-
 import styled from "styled-components";
 import { theme } from "../utils/theme";
 
-const OuterScreenBlockPadding = styled.div`
+export const OuterScreenBlockPadding = styled.div`
   padding: 1rem 2rem;
   box-sizing: border-box;
   height: 100%;
@@ -133,7 +11,7 @@ const OuterScreenBlockPadding = styled.div`
   }
 `;
 
-const PageTitle = styled.div`
+export const PageTitle = styled.div`
   font-family: "Montserrat", serif;
   font-style: normal;
   font-weight: 700;
@@ -152,18 +30,18 @@ type ScreenOuterBlockProps = {
   width?: number;
 };
 
-const ScreenOuterBlock = styled.div<ScreenOuterBlockProps>`
+export const ScreenOuterBlock = styled.div<ScreenOuterBlockProps>`
   margin: 0 auto;
   max-width: ${({ width }) => width ?? 1200}px;
 `;
 
-const globalRadiusWhite = `
+export const globalRadiusWhite = `
   border-radius: 8px;
   background: #ffffff;
   box-shadow: 0 4px 20px #e6e6e6;
 `;
 
-const ScreenWhiteBlock = styled.div`
+export const ScreenWhiteBlock = styled.div`
   padding: 2rem 3rem;
   box-sizing: border-box;
   ${globalRadiusWhite};
@@ -173,19 +51,19 @@ const ScreenWhiteBlock = styled.div`
   }
 `;
 
-const TableHolder = styled.div`
+export const TableHolder = styled.div`
   margin-top: 12px;
   overflow-x: auto;
 `;
 
-const PageHeader = styled.div`
+export const PageHeader = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 8px;
 `;
 
-const Alert = styled.div`
+export const Alert = styled.div`
   background-color: ${theme.primary["orange-vivid-700"]};
   display: flex;
   padding: 10px 15px;
@@ -196,7 +74,7 @@ const Alert = styled.div`
   margin-bottom: 1rem;
   box-sizing: border-box;
 `;
-const ColumnContainer = styled.div`
+export const ColumnContainer = styled.div`
   width: 700px;
   border-radius: 10px;
 
@@ -204,7 +82,7 @@ const ColumnContainer = styled.div`
     width: 100%;
   }
 `;
-const PostsContainer = styled.div`
+export const PostsContainer = styled.div`
   padding: 28px 46px;
   background-color: #fff;
   box-sizing: border-box;
@@ -213,7 +91,7 @@ const PostsContainer = styled.div`
     padding: 28px 0px 20px 20px;
   }
 `;
-const PostBlockContainer = styled.div`
+export const PostBlockContainer = styled.div`
   box-shadow: 0 4px 33px rgba(168, 198, 207, 0.15);
   border-radius: 10px 10px 0 0;
   background-color: #fff;
@@ -224,7 +102,7 @@ const PostBlockContainer = styled.div`
     box-sizing: border-box;
   }
 `;
-const BlackButton = styled.div`
+export const BlackButton = styled.div`
   background: #000;
   border-radius: 15px;
   font-family: "Audiowide", serif;
